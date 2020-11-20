5b63acc35acf1f058020dae56c0e5f1a65d18739
Toshimi Ataku <ATAKU@jp.ibm.com>
2020-11-20 10:10:40 +0900

    Initial Commit

---
+++ MySoundPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySoundPlayer
+//
+//  Created by Toshimi Ataku on 2020/11/20.
+//  Copyright © 2020 Toshimi Ataku. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MySoundPlayer/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  MySoundPlayer
+//
+//  Created by Toshimi Ataku on 2020/11/20.
+//  Copyright © 2020 Toshimi Ataku. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
